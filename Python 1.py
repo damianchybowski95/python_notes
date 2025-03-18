6bcfb97995eda3f694f61e28aae67db8efe2241c
Damian Chybowski <damianchybowski@gmail.com>
2025-03-18 11:18:26 +0100

    init notatki

---
+++ Python 1.py
@@ -0,0 +1,29 @@
+import sys        # Provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter
+import math       # Provides access to mathematical functions like sqrt, sin, cos, etc.
+import random     # Implements pseudo-random number generators for various distributions
+import threading  # Provides higher-level threading interface
+import time       # Provides various time-related functions
+from functools import reduce  # Provides higher-order functions that act on or return other functions
+
+
+# Print to the console
+print("Hello, World!")
+name = input("What is your name? ") # Get user input
+print("Hello, " + name.capitalize() + "!") # Print a greeting to the user
+
+
+# Poprawnie formatowane polecenia
+v1 = "1"; v2 = 2; v1 = 3
+# print( f"print v1: {v1}" )
+
+"""
+To jest komentarz, który może być wieloliniowy
+"""
+v2 = (
+    1
+    + 2
+    + 3
+)
+# print( "V2 = ", v2 )
+
+
